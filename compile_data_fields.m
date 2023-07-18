1044ae462691f4dd0d20016084531516e6941acc
kaufkj44 <kaufmankj@gmail.com>
2023-07-18 08:12:22 -0400

    added comments and additional options in is_responsive

--- compile_data_fields.m
+++ compile_data_fields.m
@@ -3,14 +3,14 @@ function [neural_struct, pupil_struct] = compile_data_fields(p, d, spont_window,
     s = mean(d.spike_traces(:,resp_window,:), 2);
     z = mean(d.spike_zscores(:,resp_window,:), 2);
     
-    spikes = reshape_by_stimulus(s, d.inner_index, []);
+    spikes = reshape_by_stimulus(s, d.inner_index, []);   % [cells * stim * reps]
     %spikes = nan(size(s,1), size(s,2), trials);
     %spikes(:,:,1:size(s,3)) = s;
     
-    zscores = reshape_by_stimulus(z, d.inner_index, []);
+    zscores = reshape_by_stimulus(z, d.inner_index, []);  % [cells * stim * reps]
     %zscores = nan(size(z,1), size(z,2), trials);
     %zscores(:,:,1:size(z,3)) = z;
-    resp = is_responsive(d, 7)';
+    resp = is_responsive(d, 7, resp_window, spont_window)';
 
 
     p = squeeze(nanmean(p(spont_window,:),1));
